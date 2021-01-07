9534e6c378d10dff49fac241c8ae730003c30df1
Izadi <64izadi@gmail.com>
2021-01-07 14:05:46 +0900

    reproduction repo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <VueCal />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VueCal from 'vue-cal';
+import 'vue-cal/dist/vuecal.css';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  components: {
+    VueCal,
+  },
+});
+</script>
